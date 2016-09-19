ce4634d055dfc0ad9ce74b9041259d3b52a7c5ff
ruslan.palagin <ruslan.palagin@masterofcode.com>
2016-09-19 21:17:21 +0200

    init

---
+++ config/application.rb
@@ -0,0 +1,37 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Paranoya
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helpers         false
+    end
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+    config.i18n.available_locales = [:ru]
+    config.i18n.default_locale = :ru
+
+    config.active_job.queue_adapter = :sidekiq
+  end
+end
