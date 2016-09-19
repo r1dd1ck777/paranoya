8ec861b952b7ae494acd3228a00ef72ae41e4c53
ruslan.palagin <ruslan.palagin@masterofcode.com>
2016-09-19 22:17:51 +0200

    #seed activeadmin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def is_admin?
+    true
+    # self.email && ENV['ADMIN_EMAILS'].to_s.include?(self.email)
+  end
 end
