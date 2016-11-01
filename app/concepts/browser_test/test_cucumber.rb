2371bc1d9a2eee1e3c9c123fedae27a7e7a1f8a6
ruslan.palagin <ruslan.palagin@masterofcode.com>
2016-11-01 18:01:08 +0200

    #cucumber run cucumber in tmp dir

--- app/concepts/browser_test/test_cucumber.rb
+++ app/concepts/browser_test/test_cucumber.rb
@@ -2,23 +2,55 @@ require 'cucumber'
 
 class BrowserTest::TestCucumber
   class << self
-    def call
+    def call browser_test = nil
       setup
-      Cucumber::Runtime.new(configuration).run!
+      file_name = "#{SecureRandom.uuid}.feature"
+      dir = tmp_dir __default_company_id
+      file = [dir, file_name].join '/'
+      write_file file, __default_content
+      Cucumber::Runtime.new(configuration(dir)).run!
+      File.delete file
+
       teardown
     end
 
-    def configuration
-      result = Cucumber::Configuration.new({paths: ["#{Rails.root}/features"], formats: [['pretty', STDOUT]]})
+    protected
+
+    def __default_company_id
+      1
+    end
+
+    def __default_content
+      <<TEXT
+@javascript
+Feature: hello world
+
+  Scenario: Show hello world
+    Then I say hello my world!
+
+TEXT
+    end
+
+    def write_file file, content
+      dir = file.split('/')[0..-2].join('/')
+      p file
+      p "mkdir -p #{dir}"
+      system "mkdir -p #{dir}"
+
+      File.write file, content
+    end
+
+    def configuration dir
+      result = Cucumber::Configuration.new({paths: [dir], formats: [['pretty', STDOUT]]})
       result
     end
 
     def setup
-      Cucumber::Configuration.class_eval do
-        def paths
-          ["#{Rails.root}/features"]
-        end
-      end
+    end
+
+    def tmp_dir company_id
+      time = Time.now
+      "#{Rails.root}/tmp/features/#{time.year}/#{time.month}/#{time.day}/#{company_id}"
     end
 
     def teardown
